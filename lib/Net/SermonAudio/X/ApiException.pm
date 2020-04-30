3ecf7b491abebf4f269046a7b51df4ac18deea65
Ben Herweyer <benjamin.herweyer@gmail.com>
2020-04-30 15:41:33 -0400

    Use Data::Dumper to display exception messages

--- lib/Net/SermonAudio/X/ApiException.pm
+++ lib/Net/SermonAudio/X/ApiException.pm
@@ -1,4 +1,5 @@
 package Net::SermonAudio::X::ApiException;
+use Data::Dumper;
 use Moo;
 use overload '""' => sub { shift->to_string };
 
@@ -13,7 +14,7 @@ has res => (is => 'ro', required => 1);
 sub code { shift->res->code }
 
 sub to_string($self) {
-    "Net::SermonAudio::X::ApiException: " . $self->message . "\nHTTP status: " . $self->code . ' ' . $self->res->message;
+    "Net::SermonAudio::X::ApiException: " . Dumper($self->message) . "\nHTTP status: " . $self->code . ' ' . $self->res->message;
 }
 
 1;
